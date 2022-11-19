4b98cfdbda2621f2f56f9e0e1b7d1381750749cc
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-11-19 22:21:37 +0500

    javascript functions

--- 16-11/app.js
+++ 16-11/app.js
@@ -1,1 +1,39 @@
 // switch case , loop
+
+// for (let i = 1; i <= 10; i++) {
+//      for (let j = 0; j <= 20; j++) {
+//           if(j===0 ) console.log( j*2  )
+//      }
+// }
+
+
+for (let i = 1; i <= 10; i++) {
+     if(i===0 ) console.log( i*i  )
+     for (let j = 0; j <=30; j++) {
+     if(j===0 ) console.log( i*3  )
+     }
+}
+// for (let i = 1; i <= 10; i++) {
+//      if(i===0 ) console.log( i*i  )
+//      for (let j = 0; j <=40; j++) {
+//      if(j===0 ) console.log( i*4  )
+//      }
+// }
+// for (let i = 1; i <= 10; i++) {
+//      if(i===0 ) console.log( i*i  )
+//      for (let j = 0; j <=50; j++) {
+//      if(j===0 ) console.log( i*5  )
+//      }
+// }
+// for (let i = 1; i <= 10; i++) {
+//      if(i===0) console.log( i*i  )
+//      for (let j = 0; j <=60; j++) {
+//      if(j===0 ) console.log( i*6  )
+//      }
+// }
+// for (let i = 1; i <= 10; i++) {
+//      if(i===0) console.log( i*i  )
+//      for (let j = 0; j <=70; j++) {
+//      if(j===0 ) console.log( i*7  )
+//      }
+// }
---
+++ 18-11/app.js
@@ -0,0 +1,6 @@
+function say(){
+     console.log("hello function");
+}
+
+
+say()
