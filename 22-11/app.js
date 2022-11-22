8807ef3181c74c2b45cabed8674cdd3262c9582f
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-11-22 20:54:17 +0500

    object

---
+++ 22-11/app.js
@@ -0,0 +1,50 @@
+// object
+
+// - Creating objects  { ... }
+
+    // - object literals - let object = {}
+    // - object constructor - let obj = new Object()
+    // { key : value }
+    // key -> name
+
+        // let obj = {
+        //     name: 'Dostonbek',
+        //     age: 23
+        // }
+
+    // accessing keys with dots,  []
+
+        // obj.name
+        // obj['name']
+
+    //  similar-> obj[0] === pbj['0']
+
+        // obj['user name']
+
+    // [],.
+
+        // [] dynamic
+
+    // delete
+
+        // delete object.name
+
+    // make dynamic user with return function
+    // abbrivation with keys and variables
+
+        // if in obj key and value the same name we can write one
+
+            // let obj = {
+            //     name,
+            //     age
+            // }
+
+    // in -> "name" in user// true
+
+        // name in user -> returns boolean
+
+    // for loop with in // prints key
+
+        // for (let i in obj) {
+        //     console.log(i);
+        // }
