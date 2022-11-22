21cb550cc86053f0280d070dd2265556aba7706f
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-11-22 20:42:31 +0500

    switch case , loop

--- 16-11/app.js
+++ 16-11/app.js
@@ -1,3 +1,6 @@
+
+//  javasCript lesson 4
+
 // switch case , loop
 
 // for (let i = 1; i <= 10; i++) {
@@ -7,12 +10,12 @@
 // }
 
 
-for (let i = 1; i <= 10; i++) {
-     if(i===0 ) console.log( i*i  )
-     for (let j = 0; j <=30; j++) {
-     if(j===0 ) console.log( i*3  )
-     }
-}
+// for (let i = 1; i <= 10; i++) {
+//      if(i===0 ) console.log( i*i  )
+//      for (let j = 0; j <=30; j++) {
+//      if(j===0 ) console.log( i*3  )
+//      }
+// }
 // for (let i = 1; i <= 10; i++) {
 //      if(i===0 ) console.log( i*i  )
 //      for (let j = 0; j <=40; j++) {
@@ -36,4 +39,42 @@ for (let i = 1; i <= 10; i++) {
 //      for (let j = 0; j <=70; j++) {
 //      if(j===0 ) console.log( i*7  )
 //      }
+// }
+
+
+
+
+// Switch statement
+
+// same If, checks only with (===)
+
+// let grade = "good";
+
+// switch (grade) {
+//   case "nice":
+//     console.log("excelent");
+//     break;
+//   case "lower":
+//     console.log("not bad");
+//     break;
+//   default:
+//     console.log("Enter only number");
+// }
+
+// In If else statement === default in switch
+
+// For Loops
+
+// for loop
+// - for (begin; condition; step)
+// - for (; i < 3; i++) {} // 0, 1, 2
+// - for (; i < 3;) {}
+// - for (;;) {} run without limits
+// break, continue
+
+// break stops hole codes
+// continue skips one step
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
 // }
