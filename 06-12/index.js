eaae5d9a6ad05b1a474b64d6d56dcb4a2b7e0c23
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-12-05 08:14:57 +0500

    advanced array

--- 06-12/index.js
+++ 06-12/index.js
@@ -1,19 +1,19 @@
 // let n = [2, 3, 1, 7, 8, 9, 6];
-let str = ["orange", "Apple", "banana", "Kiwi"];
+// let str = ["orange", "Apple", "banana", "Kiwi"];
 
-let students = [
-  { id: 1, year: 2002, name: "Iqboljon Yuldashev" },
-  { id: 2, year: 2000, name: "Asilbek Topilov" },
-  { id: 3, year: 2002, name: "Najmiddin Nazarov " },
-  { id: 4, year: 2002, name: "Mominov Abdulloh" },
-  { id: 5, year: 1995, name: "Sardor Tursunboyev" },
-  { id: 6, year: 2002, name: "Hudoyberdieva Mohichehra " },
-  { id: 7, year: 2007, name: "Muhammad Nurmirzayev" },
-  { id: 8, year: 2002, name: "Shukurov Shohruh" },
-];
+// let students = [
+//   { id: 1, year: 2002, name: "Iqboljon Yuldashev" },
+//   { id: 2, year: 2000, name: "Asilbek Topilov" },
+//   { id: 3, year: 2002, name: "Najmiddin Nazarov " },
+//   { id: 4, year: 2002, name: "Mominov Abdulloh" },
+//   { id: 5, year: 1995, name: "Sardor Tursunboyev" },
+//   { id: 6, year: 2002, name: "Hudoyberdieva Mohichehra " },
+//   { id: 7, year: 2007, name: "Muhammad Nurmirzayev" },
+//   { id: 8, year: 2002, name: "Shukurov Shohruh" },
+// ];
 
-let res = students.group((value) => value.year);
-console.log(res);
+// let res = students.group((value) => value.year);
+// console.log(res);
 
 // let res = {
 //   2002: [
