319c56c87416457a77bf25477ed8078590d78330
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-12-11 11:59:47 +0500

    CRUD

--- 09-12/app.js
+++ 09-12/app.js
@@ -92,3 +92,16 @@
 // [1, 1, 1, 2, 2];
 // [1, 1, 1];
 // [1];
+let Object = {
+banana: 1,
+orange: 2,
+meat: 4,
+};
+
+let doublePrices = Object.fromEntries(
+// convert prices to array, map each key/value pair into another pair
+// and then fromEntries gives back the object
+Object.entries(prices).map(entry => [entry[0], entry[1] * 2])
+);
+
+alert(doublePrices.meat); // 8
