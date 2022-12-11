01aaaaea0c2de0a8a873b2e9b5e0791df4b4c9c8
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-12-11 11:54:49 +0500

    CRUD

--- 09-12/app.js
+++ 09-12/app.js
@@ -1,57 +1,57 @@
-let students = [
-  { id: 1, year: 2002, name: "Iqboljon Yuldashev" },
-  { id: 2, year: 2000, name: "Asilbek Topilov" },
-  { id: 3, year: 2002, name: "Najmiddin Nazarov " },
-  { id: 4, year: 2002, name: "Mominov Abdulloh" },
-  { id: 5, year: 1995, name: "Sardor Tursunboyev" },
-  { id: 6, year: 2002, name: "Hudoyberdieva Mohichehra " },
-  { id: 7, year: 2007, name: "Muhammad Nurmirzayev" },
-  { id: 8, year: 2002, name: "Shukurov Shohruh" },
-];
+// let students = [
+//   { id: 1, year: 2002, name: "Iqboljon Yuldashev" },
+//   { id: 2, year: 2000, name: "Asilbek Topilov" },
+//   { id: 3, year: 2002, name: "Najmiddin Nazarov " },
+//   { id: 4, year: 2002, name: "Mominov Abdulloh" },
+//   { id: 5, year: 1995, name: "Sardor Tursunboyev" },
+//   { id: 6, year: 2002, name: "Hudoyberdieva Mohichehra " },
+//   { id: 7, year: 2007, name: "Muhammad Nurmirzayev" },
+//   { id: 8, year: 2002, name: "Shukurov Shohruh" },
+// ];
 
-const updateUser = ({ id, key, value }) => {
-  let res = students.map(
-    (usr) => (usr.id === id ? { ...usr, [key]: value } : "hey")
-    // usr.id === id ? { ...usr, [key]: value } : usr
-  );
-  return (students = res);
-};
+// const updateUser = ({ id, key, value }) => {
+//   let res = students.map(
+//     (usr) => (usr.id === id ? { ...usr, [key]: value } : "hey")
+//     // usr.id === id ? { ...usr, [key]: value } : usr
+//   );
+//   return (students = res);
+// };
 
-// updateUser({ id: 3, key: "name", value: "Gulchapchap" });
+// // updateUser({ id: 3, key: "name", value: "Gulchapchap" });
 
-const addUser = (user) => {
-  return (students = [...students, user]);
-};
+// const addUser = (user) => {
+//   return (students = [...students, user]);
+// };
 
-const deleteStudent = (id) => {
-  let res = students.filter((v) => v.id !== id);
-  return (students = res);
-};
+// const deleteStudent = (id) => {
+//   let res = students.filter((v) => v.id !== id);
+//   return (students = res);
+// };
 
-const sortByAlphabet = () => {
-  let res = students.sort((a, b) => a.name.localeCompare(b.name));
-  return (students = res);
-};
+// const sortByAlphabet = () => {
+//   let res = students.sort((a, b) => a.name.localeCompare(b.name));
+//   return (students = res);
+// };
 
-const sortByYear = () => {
-  let res = students.sort((a, b) => a.year - b.year);
-  return (students = res);
-};
+// const sortByYear = () => {
+//   let res = students.sort((a, b) => a.year - b.year);
+//   return (students = res);
+// };
 
-// name = 'ov'
-const sortByInputName = (name) => {
-  let res = students.filter((value) => value.name.includes(name));
-  return (students = res);
-};
-// name = 'ov'
-const sortByInputYear = (year) => {
-  let res = students.filter((value) => `${value.year}`.includes(year));
-  return (students = res);
-};
-const Filter = ({ value: input, type }) => {
-  let res = students.filter((value) => `${value[type]}`.includes(input));
-  return (students = res);
-};
+// // name = 'ov'
+// const sortByInputName = (name) => {
+//   let res = students.filter((value) => value.name.includes(name));
+//   return (students = res);
+// };
+// // name = 'ov'
+// const sortByInputYear = (year) => {
+//   let res = students.filter((value) => `${value.year}`.includes(year));
+//   return (students = res);
+// };
+// const Filter = ({ value: input, type }) => {
+//   let res = students.filter((value) => `${value[type]}`.includes(input));
+//   return (students = res);
+// };
 
 // addUser({ id: 9, year: 2007, name: "Eshmat Eshmatov" });
 
