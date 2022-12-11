044f72e28d541ea5907e2abd5e9bcc82d79396dd
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-12-11 12:04:55 +0500

    CRUD

--- 09-12/app.js
+++ 09-12/app.js
@@ -92,25 +92,25 @@
 // [1, 1, 1, 2, 2];
 // [1, 1, 1];
 // [1];
-let Object = {
-banana: 1,
-orange: 2,
-meat: 4,
-};
+// let Object = {
+// banana: 1,
+// orange: 2,
+// meat: 4,
+// };
 
-let doublePrices = Object.fromEntries(
-// convert prices to array, map each key/value pair into another pair
-// and then fromEntries gives back the object
-Object.entries(prices).map(entry => [entry[0], entry[1] * 2])
-);
+// let doublePrices = Object.fromEntries(
+// // convert prices to array, map each key/value pair into another pair
+// // and then fromEntries gives back the object
+// Object.entries(prices).map(entry => [entry[0], entry[1] * 2])
+// );
 
-alert(doublePrices.meat); // 8
+// alert(doublePrices.meat); // 8
 
 
-let salaries = {
-     "John": 100,
-     "Pete": 300,
-     "Mary": 250
-   };
+// let salaries = {
+//      "John": 100,
+//      "Pete": 300,
+//      "Mary": 250
+//    };
    
-   alert( sumSalaries(salaries) ); // 650
+//    alert( sumSalaries(salaries) ); // 650
